c4a9c045649930bcf93a7c4b7d65ff6e8928f705
rai.cho <rai.cho@gu3.co.jp>
2017-04-14 17:07:09 +0900

    dog

---
+++ GA/Assets/SimpleDogs/Demo/BtnScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BtnScript : MonoBehaviour {
+    GameObject go1 = null;
+    Animator ator1 = null;
+
+    void Start()
+    {
+        go1 = DogController.getGameObject();
+        ator1 = go1.GetComponent<Animator>();
+        
+    }
+
+    public void OnClickEat()
+    {
+        DogController.Eat();
+    }
+
+    public void OnClickSit()
+    {
+        DogController.Sit();
+    }
+
+    public void OnClickWalk()
+    {
+        DogController.Walk();
+    }
+}
---
+++ GA/Assets/SimpleDogs/Demo/DogController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DogController : MonoBehaviour {
+    public static Animator ator1 = null;
+    public static GameObject go = null;
+    // Use this for initialization
+    void Start () {
+        ator1 = this.gameObject.GetComponent<Animator>();
+        go = this.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public static GameObject getGameObject()
+    {
+        return go;
+    }
+
+    public static void Eat()
+    {
+        ator1.Play("Eat");
+    }
+
+    public static void Bark()
+    {
+        ator1.Play("Bark");
+    }
+
+    public static void Walk()
+    {
+        ator1.Play("Walk");
+    }
+
+    public static void Sit()
+    {
+        ator1.Play("Sit");
+    }
+
+    public static void reset()
+    {
+        ator1.ResetTrigger("Eat_b");
+        ator1.ResetTrigger("Bark_b");
+        ator1.ResetTrigger("Walk_b");
+        ator1.ResetTrigger("Sit_b");
+    }
+
+}
